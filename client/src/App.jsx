fb1c0023ecfde91a2f59a88833a17f867062ad5d
PHS <haexunx@gmail.com>
2026-05-18 10:16:34 +0900

    chore: npm workspace 및 ESLint 루트 설정 통합

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import ProtectedRoute from "./router/ProtectedRoute";
 import WebContent from "./layouts/WebContent/WebContent";
 import AppWrapper from "./layouts/AppWrapper/AppWrapper";
