f6634c00b0a24d8a26039f6efc41148b70b3dbd1
PHS <haexunx@gmail.com>
2026-05-19 16:23:38 +0900

    feat: Header 구현

--- client/src/layouts/AppMainLayout/AppMainLayout.jsx
+++ client/src/layouts/AppMainLayout/AppMainLayout.jsx
@@ -1,5 +1,5 @@
-import Footer from "client/src/components/Footer";
-import Header from "client/src/components/Header";
+import Footer from "@components/Footer";
+import Header from "@components/Header/Header";
 import { Outlet } from "react-router-dom";
 
 const AppMainLayout = () => {
